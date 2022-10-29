78320bc4b1a0cf20b01ee9681e3ad96f205a3715
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-10-29 15:19:30 -0700

    completed models minus time format getter

---
+++ models/Reaction.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const { reactionSchema } = require('./Reaction');
+
+
+// Schema to create Thought model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+  reactionSchema.methods.getTimeFormat = function () {
+    //put code to format time stamp here
+  }
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const { reactionSchema } = require('./Reaction');
+
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+      min_length: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    //???username might not be setup correctly????
+    username: {
+        type: String,
+        required: true,
+      },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    },
+  }
+);
+
+thoughtSchema.methods.getTimeFormat = function () {
+  //put code to format time stamp here
+}
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
